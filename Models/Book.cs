4d1c6aba6031a493d548bd0200918e89bdbbcc28
dsrasband@gmail.com <dsrasband@gmail.com>
2024-03-22 19:53:44 -0600

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission11_Rasband.Models;
+
+public partial class Book
+{
+    [Key]
+    public int BookId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public string Publisher { get; set; }
+
+    public string ISBN { get; set; }
+
+    public string Classification { get; set; }
+
+    public string Category { get; set; }
+
+    public int PageCount { get; set; }
+
+    public double Price { get; set; }
+}
---
+++ Models/IBookRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Mission11_Rasband.Models
+{
+    public interface IBookRepository
+    {
+        public IQueryable<Book> Books { get; }
+    }
+}
